3b02460b568acec115d2d9954b10284ce1a527f9
Harenabs21 <hei.harena.6@gmail.com>
2024-06-05 23:55:36 +0300

    feat: add recommandation section

--- src/app/(front-office)/home/page.tsx
+++ src/app/(front-office)/home/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/app/components/hero/hero";
+import Recommandation from "@/app/components/recommandations";
 
 export default function Home() {
   return (
     <div>
         <Hero />
+        <Recommandation />
     </div>
   )
 }
