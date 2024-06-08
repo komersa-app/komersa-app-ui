abcae276c1efa39a573afb405a1bfb9664f4ee33
Harenabs21 <hei.harena.6@gmail.com>
2024-06-08 10:13:23 +0300

    update the url  of the image and car

--- src/app/(front-office)/cars/[id]/page.tsx
+++ src/app/(front-office)/cars/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Loader from "@/app/components/loader/loader";
-import { CarControllerApi, CarDtoResponse } from "@/app/components/provider/typescript-client";
+import { ImagesControllerApi, ImagesDtoResponse } from "@/app/components/provider/typescript-client";
 import { useEffect, useState } from "react";
 
 interface detailsProps {
@@ -11,15 +11,15 @@ interface detailsProps {
 }
 export default function CardDetails({params} : detailsProps) {
     const { id } = params;
-    const [carDetails, setCarDetails] = useState<CarDtoResponse>();
+    const [carDetails, setCarDetails] = useState<ImagesDtoResponse>();
     const [isLoading, setIsLoading] = useState(false);
-    const carControllerApi = new CarControllerApi();
+    const carControllerApi = new ImagesControllerApi();
 
     useEffect(() =>{
         const fetchCarDetails = async () => {
             setIsLoading(true)
             try {
-                await carControllerApi.getCarById(id).then(response =>{
+                await carControllerApi.getImagesById(id).then(response =>{
                     setCarDetails(response.data);
                 })
                 
@@ -39,38 +39,41 @@ export default function CardDetails({params} : detailsProps) {
             {isLoading ? (
                 <div className="flex m-auto items-center justify-center"> <Loader /> </div>
             ):(
-                <div>
-                    <div className="fle flex-col gap-5 ">
-                        <h1 className="lg:text-4xl">{`${carDetails?.details?.brand} ${carDetails?.details?.model} ${carDetails?.name}`} </h1>
-                        <h1 className="lg:text-4xl font-semibold text-indigo-700"> {`$${carDetails?.price?.amount}`} </h1>
-                        <div className="card-compact w-96 rounded-md shadow-sm">
+                <div className="container flex mx-auto gap-10 flex-wrap p-8">
+                    <div className="max-w-lg">
+                        <img src={carDetails?.url} alt="car" />
+                    </div>
+                    <div className="block mt-5 ">
+                        <h1 className="lg:text-4xl">{`${carDetails?.car?.details?.brand} ${carDetails?.car?.details?.model} ${carDetails?.car?.name}`} </h1>
+                        <h1 className="lg:text-4xl font-semibold text-indigo-700"> {`$${carDetails?.car?.price?.amount}`} </h1>
+                        <div className="card w-96 p-3 rounded-md border-2 border-gray-400 shadow-sm">
                             <div className="flex justify-between">
-                                <span className="font-[400]"> Make </span>
-                                <span>{carDetails?.details?.brand}</span>
+                                <h2 className="font-[400]"> Make </h2>
+                                <h2>{carDetails?.car?.details?.brand}</h2>
                             </div>
                             <div className="flex justify-between">
-                                <span className="font-[400]"> Name </span>
-                                <span>{carDetails?.name}</span>
+                                <h2 className="font-[400]"> Name </h2>
+                                <h2>{carDetails?.car?.name}</h2>
                             </div>
                             <div className="flex justify-between">
-                            <span className="font-[400]"> Model </span>
-                                <span>{carDetails?.details?.model}</span>
+                            <h2 className="font-[400]"> Model </h2>
+                                <h2>{carDetails?.car?.details?.model}</h2>
                             </div>
                             <div className="flex justify-between">
-                            <span className="font-[400]"> Color </span>
-                                <span>{carDetails?.color}</span>
+                            <h2 className="font-[400]"> Color </h2>
+                                <h2>{carDetails?.car?.color}</h2>
                             </div>
                             <div className="flex justify-between">
-                            <span className="font-[400]"> Fuel type </span>
-                                <span>{carDetails?.motorType}</span>
+                            <h2 className="font-[400]"> Fuel type </h2>
+                                <h2>{carDetails?.car?.motorType}</h2>
                             </div>
                             <div className="flex justify-between">
-                            <span className="font-[400]"> Power </span>
-                                <span>{carDetails?.power}</span>
+                            <h2 className="font-[400]"> Power </h2>
+                                <h2>{carDetails?.car?.power}</h2>
                             </div>
                             <div className="flex justify-between">
-                            <span className="font-[400]"> description </span>
-                                <span className="text-wrap">{carDetails?.description}</span>
+                            <h2 className="font-[400]"> description </h2>
+                                <h2 className="text-wrap">{carDetails?.car?.description}</h2>
                             </div>
                         </div>
                     </div>
