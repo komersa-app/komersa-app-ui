6b42bb7ce9f2b3660ec9880e2ad512e3a6e9bfb5
Harenabs21 <hei.harena.6@gmail.com>
2024-06-11 20:39:43 +0300

    chore: transform as function

--- src/provider/api.ts
+++ src/provider/api.ts
@@ -1,11 +1,11 @@
 import { AdminControllerApi, AppointmentControllerApi, CarControllerApi, DetailsControllerApi, ImagesControllerApi, LoginControllerApi, PricesControllerApi, VisitorControllerApi } from "./typescript-client";
 
 
-export const detailsApi = new DetailsControllerApi();
-export const priceApi = new PricesControllerApi();
-export const carApi = new CarControllerApi();
-export const imageApi = new ImagesControllerApi();
-export const adminApi =  new AdminControllerApi();
-export const appointmentApi = new AppointmentControllerApi();
-export const visitorApi = new VisitorControllerApi();
-export const logintApi = new LoginControllerApi();
+export const detailsApi = () => new DetailsControllerApi();
+export const priceApi = () => new PricesControllerApi();
+export const carApi = () => new CarControllerApi();
+export const imageApi = () => new ImagesControllerApi();
+export const adminApi = () =>  new AdminControllerApi();
+export const appointmentApi = () => new AppointmentControllerApi();
+export const visitorApi = () => new VisitorControllerApi();
+export const logintApi = () => new LoginControllerApi();
