ee8714793d1b59172fcfbdf4dbe2e605efbfdd8c
Harenabs21 <hei.harena.6@gmail.com>
2024-06-12 13:41:49 +0300

    feat: add show component

--- src/components/AdminApp.tsx
+++ src/components/AdminApp.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
 import { Admin, Login, Resource } from 'react-admin';
 import simpleRestProvider from 'ra-data-simple-rest';
-import { UserCreate } from './user/UserCreate';
-import UserEdit from './user/UserEdit';
-import { UserList } from './user/UserList';
-import authProvider from '../app/admin/authProvider';
+import { UserCreate } from './dashboard/user/UserCreate';
+import { UserEdit }from './dashboard/user/UserEdit';
+import { UserList } from './dashboard/user/UserList';
+import authProvider from '../provider/authProvider';
+import { UserShow } from './dashboard/user/UserShow';
 
 const dataProvider = simpleRestProvider("http://localhost:8080");
 
-const AdminApp: React.FC = () => (
+const AdminApp = () => (
   <Admin loginPage={Login} authProvider={authProvider} dataProvider={dataProvider}>
-    <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} />
+    <Resource name="admins" recordRepresentation={'admin'} list={UserList} edit={UserEdit} create={UserCreate} show={UserShow} hasCreate/>
   </Admin>
 );
 
