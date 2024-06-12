3aae38d1ed32ac55062f61c03e9415c66b4a123b
Harenabs21 <hei.harena.6@gmail.com>
2024-06-12 22:26:11 +0300

    create a custom data provider template

---
+++ src/provider/type.ts
@@ -0,0 +1,7 @@
+export type CustomDataProvider = {  
+  getList: (page?: number, perPage?: number, filter?: any, meta?: any) => Promise<any>;
+  getOne: (id: string, meta?: any) => Promise<any>;
+  save: (resources: any, meta?: any) => Promise<any>;
+  update: (id: string, resources: any, meta?: any) => Promise<any>;
+  delete: (id: string) => Promise<any>;    
+}
